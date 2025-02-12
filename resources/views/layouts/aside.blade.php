c8e93c2c047f728c34a6e86359258e0818fdeff8
Win-Lwin <winlwin.developer@gmail.com>
2025-02-12 12:18:39 +0630

    Layout: design admin panel

---
+++ resources/views/layouts/aside.blade.php
@@ -0,0 +1,18 @@
+<aside class="min-h-screen border-r border-gray-200/20">
+    <!-- <label for="my-drawer" aria-label="close sidebar" class=""></label> -->
+    <ul class="menu space-y-2 dark:bg-gray-800 text-base-content min-h-full w-60 p-4">
+        <div class="px-4">
+            <a href="{{ route('dashboard') }}">
+                <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+            </a>
+        </div>
+        <li><a>Movie Management</a></li>
+        <li><a>Screen Management</a></li>
+        <li><a>Showtime Management</a></li>
+        <li><a>Seat Management</a></li>
+        <li><a>Booking Management</a></li>
+        <li><a>Payment Management</a></li>
+        <li><a>Notification Management</a></li>
+        <li><a>Reviews & Rating</a></li>
+    </ul>
+</aside>
